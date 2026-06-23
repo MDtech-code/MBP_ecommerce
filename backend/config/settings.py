18010c8a9db8f6a1a41bbfe043543911314dc5f1
Mudasir ali <143800687+MDtech-code@users.noreply.github.com>
2026-06-23 13:33:27 +0500

    Add email service configuration to settings.py for SMTP support

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -243,6 +243,15 @@ CELERY_TIMEZONE = 'Asia/Karachi'
 
 
 
+# ─── Email Service Configuration ──────────────────────────────────────────
+#! this line of help to configore the default base email request sending 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST =env('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT =env.int('EMAIL_PORT', default=587)
+EMAIL_USE_TLS =env.bool('EMAIL_USE_TLS', default=True)
+EMAIL_HOST_USER =env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # ─── Logging ──────────────────────────────────────────
 LOGGING = {
     'version': 1,
