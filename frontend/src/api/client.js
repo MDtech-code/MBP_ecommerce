6912bb5ede36d88ea6f1b7a1a476a2529d8a8538
Mudasir ali <143800687+MDtech-code@users.noreply.github.com>
2026-06-27 12:37:44 +0500

    add .env.docker in gitignore and remove relative path in baseurl

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_ORIGIN,
+  baseURL:'',
   headers: {
     "Content-Type": "application/json",
   },
