bb02d08c97e0c28f3a35890bc73d8e09752cd5f0
Mudasir ali <143800687+MDtech-code@users.noreply.github.com>
2026-07-24 15:06:06 +0500

    remove console statement

--- frontend/src/widgets/header/ui/Header.jsx
+++ frontend/src/widgets/header/ui/Header.jsx
@@ -3,7 +3,6 @@ import MainNavbar from "./MainNavbar";
 import { useAuthStore } from "@entities/user"
 export default function Header(){
     const user = useAuthStore((state) => state.user)
-    console.log(`mia navbar sa console ara hu ya user ha ${user}`)
 return (
 <header>
  {!user && <TopBar />}
