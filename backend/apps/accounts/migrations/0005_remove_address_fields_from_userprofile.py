4a29342ea680be8f0f75c4841dd7d5cf29102729
MDtech-code <mudasirock001@gmail.com>
2026-07-26 22:33:45 -0700

    add new database test_db and modified old migration according to current structure need

--- backend/apps/accounts/migrations/0005_remove_address_fields_from_userprofile.py
+++ backend/apps/accounts/migrations/0005_remove_address_fields_from_userprofile.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0004_copy_userprofile_address_to_useraddress'),
+        ('accounts', '0003_add_useraddress_model'),
     ]
 
     operations = [
