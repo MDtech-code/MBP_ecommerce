7aaf3747149851ce2ad9315a78b8367a211e7e58
MDtech-code <mudasirock001@gmail.com>
2026-07-29 06:19:27 -0700

    now barrel configure with the wishlistservice

--- frontend/src/shared/api/services/index.js
+++ frontend/src/shared/api/services/index.js
@@ -3,3 +3,4 @@ export { cartService } from "./cartService";
 export { productService } from "./productService";
 export {orderService} from "./orderService";
 export {reviewService} from './reviewService';
+export {wishlistService} from "./wishlistService"
