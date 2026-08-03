9ed8d28f07d109d4b6ea7bc24a67efaeef2bd707
MDtech-code <mudasirock001@gmail.com>
2026-08-03 12:56:50 -0700

    adjust formating

--- frontend/src/shared/ui/FormInput/FormInput.jsx
+++ frontend/src/shared/ui/FormInput/FormInput.jsx
@@ -15,34 +15,31 @@ export default function FormInput({
 
   return (
     <div className="flex flex-col gap-1">
-    <div className="flex items-center gap-3 border border-gray-200 rounded-lg px-4 py-3 focus-within:border-primary  dark:border-gray-700"> 
-      {Icon && <Icon size={20} className="text-gray-400 shrink-0" />}
+      <div className="flex items-center gap-3 border border-gray-200 rounded-lg px-4 py-3 focus-within:border-primary  dark:border-gray-700">
+        {Icon && <Icon size={20} className="text-gray-400 shrink-0" />}
 
-      <input
-        type={isPassword && showPassword ? "text" : type}
-        placeholder={placeholder}
-        name={name}
-        value={value}
-        onChange={onChange}
-        className="w-full outline-none text-gray-900 placeholder-gray-400
-    dark:text-white dark:placeholder-gray-300"
-      />
+        <input
+          type={isPassword && showPassword ? "text" : type}
+          placeholder={placeholder}
+          name={name}
+          value={value}
+          onChange={onChange}
+          className="w-full outline-none text-gray-900 placeholder-gray-400
+         dark:text-white dark:placeholder-gray-300"
+        />
 
-      {isPassword && (
-        <button type="button" onClick={() => setShowPassword(!showPassword)}>
-          {showPassword ? (
-            <EyeOff size={20} className="text-gray-400" />
-          ) : (
-            <Eye size={20} className="text-gray-400" />
-          )}
-        </button>
-      )}
-    </div>
-     {/* Field error — only renders when error exists */}
-      {error && (
-        <p className="text-xs text-red-500 pl-1">{error}</p>
-      )}
+        {isPassword && (
+          <button type="button" onClick={() => setShowPassword(!showPassword)}>
+            {showPassword ? (
+              <EyeOff size={20} className="text-gray-400" />
+            ) : (
+              <Eye size={20} className="text-gray-400" />
+            )}
+          </button>
+        )}
+      </div>
+      {/* Field error — only renders when error exists */}
+      {error && <p className="text-xs text-red-500 pl-1">{error}</p>}
     </div>
   );
 }
-
