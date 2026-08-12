18374c424c69d61e8be517dc161590e4aec8ea3a
MDtech-code <mudasirock001@gmail.com>
2026-08-12 09:59:11 -0700

    update barrel file so full extract so we actually use the extract pagniation method

--- frontend/src/shared/api/index.js
+++ frontend/src/shared/api/index.js
@@ -1,6 +1,6 @@
 export * from "./services";
 export { client } from "./graphqlClient";
 export { setupInterceptors } from "./interceptors";
-export { normalizeError,extractErrors,ErrorCode } from "./transformers";
+export { normalizeError,extractErrors,extractPagination,ErrorCode } from "./transformers";
 export { api } from './client';
 export {ensureCsrfToken} from "./csrf";
